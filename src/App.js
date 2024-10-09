49f3f688d09e4812c69345a23d9c6aa22a56bf92
ninatylichko <ninatylichko@gmail.com>
2024-10-09 17:11:12 +0300

    Added my files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Members from "./members.js";
+function App() {
+  return (
+    <div className="App">
+      <h1>Rosca App</h1>
+      
+      <p>Welcome to the Rosca App. You can see our members list below:</p>
+       <Members />
+    </div>
+  );
+}
+
+export default App;
